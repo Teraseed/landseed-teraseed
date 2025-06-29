0c8a0827b78b835fd1b5dcdf67de687d5e7b6aa0
mashlahache <m.perron345@gmail.com>
2025-06-29 15:32:11 -0400

    fixed the height of iframe

--- src/app/[locale]/technicalBooklet/page.tsx
+++ src/app/[locale]/technicalBooklet/page.tsx
@@ -6,8 +6,8 @@ export default function TechnicalBooklet() {
     const params = useParams();
 
     return (
-        <div>
-            <embed className="pointer-events-none" src={`/downloads/technical_booklet_${params.locale}.pdf#toolbar=0&navpanes=0&scrollbar=0&statusbar=0&messages=0&scrollbar=0`} width="100%" height={1500}></embed>
+        <div className="pointer-events-none">
+            <iframe className="pointer-events-none" src={`/downloads/technical_booklet_${params.locale}.pdf#toolbar=0&navpanes=0&scrollbar=0&statusbar=0&messages=0&scrollbar=0`} width="100%" height={5500}></iframe>
         </div>
 
     )
