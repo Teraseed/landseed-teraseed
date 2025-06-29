8fef29f9fb047f32f5a8faf4b531d9aa4db2e241
mashlahache <m.perron345@gmail.com>
2025-06-29 15:18:32 -0400

    removed the ability to download and print the pdf booklet

--- src/app/[locale]/technicalBooklet/page.tsx
+++ src/app/[locale]/technicalBooklet/page.tsx
@@ -6,7 +6,9 @@ export default function TechnicalBooklet() {
     const params = useParams();
 
     return (
-        <embed src={`/downloads/technical_booklet_${params.locale}.pdf#toolbar=0&navpanes=0&scrollbar=0&statusbar=0&messages=0&scrollbar=0`} width="100%" height={1500}></embed>
+        <div>
+            <embed className="pointer-events-none" src={`/downloads/technical_booklet_${params.locale}.pdf#toolbar=0&navpanes=0&scrollbar=0&statusbar=0&messages=0&scrollbar=0`} width="100%" height={1500}></embed>
+        </div>
 
     )
 
